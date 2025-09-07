5ed67e162acc7a7be1fd5981e423eb387a86e93c
ARULKUMAR <101092540+ARULKUMAR0106@users.noreply.github.com>
2025-09-07 16:29:45 +0530

    change path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: mode=="production"?"/fijf-luxe-container/":"",
+   base: mode=="production"?"/fiji-luxe-container/":"",
   server: {
     host: "::",
     port: 8080,
